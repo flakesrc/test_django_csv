139817a6a745d1e9b4dbdc273136244db1c28ec4
Marcos Souza <flakesrc@protonmail.com>
2021-02-10 14:19:23 -0300

    criacao e migracao app olimpiadas

--- config/settings/base.py
+++ config/settings/base.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "rest_framework",
     # my apps
     "users",
+    "olympic",
 ]
 
 MIDDLEWARE = [
---
+++ olympic/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+
+class Athlete(models.Model):
+    name = models.CharField(max_length=80)
+    sex = models.CharField(max_length=1)
+    age = models.IntegerField()
+    height = models.IntegerField()
+    weight = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    athlete = models.ManyToManyField(Athlete)
+
+    name = models.CharField(max_length=80)
+    noc = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    NA = 0
+    BRONZE = 1
+    SILVER = 2
+    GOLD = 3
+
+    MEDAL_CHOICES = [
+        (NA, "NA"),
+        (BRONZE, "Bronze"),
+        (SILVER, "Silver"),
+        (GOLD, "Gold"),
+    ]
+
+    athele = models.ManyToManyField(Athlete)
+    team = models.ManyToManyField(Team)
+
+    year = models.IntegerField()
+    season = models.CharField(max_length=80)
+    city = models.CharField(max_length=80)
+    sport = models.CharField(max_length=80)
+    event = models.CharField(max_length=80)
+    medal = models.IntegerField(choices=MEDAL_CHOICES)
+
+    def __str__(self):
+        return self.season
