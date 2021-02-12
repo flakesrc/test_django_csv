e01e758fe02942eff2f253811b3e5afe44c095bb
Marcos Souza <flakesrc@protonmail.com>
2021-02-12 14:37:33 -0300

    ajustes para usar pandas e ajustar valores invalidos

--- populate_bulk.py
+++ populate_bulk.py
@@ -7,7 +7,7 @@
 
 import os
 import django
-import csv
+import pandas as pd
 from django.conf import settings
 from django.db.utils import IntegrityError
 
@@ -18,71 +18,72 @@ from olympic.models import Game, Athlete
 
 
 def populate():
-    with open("assets/athlete_events.csv") as athlete_events:
-        reader = csv.reader(athlete_events)
+    athlete_list = pd.read_csv("assets/athlete_events.csv")
 
-        # pula a row com o nome dos campos csv
-        next(reader)
+    df = pd.DataFrame(
+        athlete_list,
+        columns=[
+            "ID",
+            "Name",
+            "Sex",
+            "Age",
+            "Height",
+            "Weight",
+            "Team",
+            "NOC",
+            "Games",
+            "Year",
+            "Season",
+            "City",
+            "Sport",
+            "Event",
+            "Medal",
+        ],
+    )
 
-        athlete_objs = []
-        game_objs = []
+    athlete_objs = []
+    game_objs = []
 
-        for i, rows in enumerate(reader):
+    for row in df.itertuples():
 
-            # número da linha do registro no csv
-            # usado para identificar o game e usar como id
-            register_row_number = i + 1
+        # número da linha de registros de jogos
+        # começa no index 2 do csv
+        register_row_number = row.Index + 2
 
-            pk = rows[0]
-            name = rows[1]
-            sex = rows[2]
-            age = rows[3] if rows[3] != "NA" else None
-            height = rows[4] if rows[4] != "NA" else None
-            weight = float(rows[5]) if rows[5] != "NA" else None
-            team = rows[6]
-            noc = rows[7]
-            game_name = rows[8]
-            year = rows[9]
-            season = rows[10]
-            city = rows[11]
-            sport = rows[12]
-            event = rows[13]
-            medal = rows[14]
+        athlete_instance = Athlete(
+            id=row.ID,
+            name=row.Name,
+            sex=row.Sex,
+            age=row.Age,
+            height=row.Height if str(row.Height) != "nan" else None,
+            weight=row.Weight if str(row.Weight) != "nan" else None,
+            team=row.Team,
+            noc=row.NOC,
+        )
+        athlete_objs.append(athlete_instance)
 
-            athlete_instance = Athlete(
-                id=pk,
-                name=name,
-                sex=sex,
-                age=age,
-                height=height,
-                weight=weight,
-                team=team,
-                noc=noc,
-            )
+        game_instance = Game(
+            id=register_row_number,
+            athlete_id_ref=athlete_instance.id,
+            name=row.Games,
+            year=row.Year,
+            season=row.Season,
+            city=row.City,
+            sport=row.Sport,
+            event=row.Event,
+            medal=row.Medal if str(row.Medal) != "nan" else None,
+        )
+        game_objs.append(game_instance)
 
-            athlete_objs.append(athlete_instance)
-
-            game_objs.append(
-                Game(
-                    id=register_row_number,
-                    name=game_name,
-                    year=year,
-                    season=season,
-                    city=city,
-                    sport=sport,
-                    event=event,
-                    medal=medal,
-                )
-                .save()
-                .athlete.add(athlete_instance)
-            )
-
-        Athlete.objects.bulk_create(athlete_objs, ignore_conflicts=True)
-        Game.objects.bulk_create(game_objs, ignore_conflicts=True)
+    # adiciona as instancias no banco de dados
+    # ignore para ignorar se já existir um registro
+    # caso o script seja pausado e executado posteriormente
+    # Athlete.objects.bulk_create(athlete_objs, ignore_conflicts=True)
+    # Game.objects.bulk_create(game_objs, ignore_conflicts=True)
 
 
 if __name__ == "__main__":
-    print("\nPopulando campos a partir do csv...")
+    print("\nPopulando campos a partir do csv...\n")
 
     populate()
 
