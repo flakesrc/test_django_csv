62961e0674712631342da8f1eadbc39fdbd119d7
Marcos Souza <flakesrc@protonmail.com>
2021-02-16 10:51:10 -0300

    add host e post config prod

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -16,7 +16,7 @@ DATABASES = {
         "NAME": config("DB_NAME"),
         "USER": config("DB_USER"),
         "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("HOST"),
-        "PORT": config("PORT"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
     }
 }
