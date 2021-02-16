e0c24af2425760d1cedff98585b9a9e9648db406
Marcos Souza <flakesrc@protonmail.com>
2021-02-16 11:00:14 -0300

    add dj database url

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 from decouple import config
-
+import dj_database_url
 
 SECRET_KEY = config("SECRET_KEY")
 
@@ -10,13 +10,4 @@ ALLOWED_HOSTS = [
     "",
 ]
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST"),
-        "PORT": config("DB_PORT"),
-    }
-}
+DATABASES = {"default": dj_database_url.parse(config("DATABASE_URL"))}
